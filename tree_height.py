907767bad5bbd8d6375d4317c9408dd49bddeb7d
Bodya <hocaleangu@gmail.com>
2023-03-05 16:31:02 +0200

    'hi'

--- tree_height.py
+++ tree_height.py
@@ -1,33 +1,52 @@
-# python3
-
 import sys
 import threading
 import numpy
 
 
 def compute_height(n, parents):
-    # Write this function
-    max_height = 0
-    # Your code here
-    return max_height
+    children = [[] for _ in range(n)]
+    for i in range(n):
+        parent = parents[i]
+        if parent == -1:
+            root = i
+        else:
+            children[parent].append(i)
+
+    def compute_depth(node): #gets max height
+        if not children[node]:
+            return 1
+        max_depth = 0
+        for child in children[node]:
+            depth = compute_depth(child)
+            max_depth = max(max_depth, depth)
+        return max_depth + 1
+
+    return compute_depth(root) #max height
 
 
 def main():
-    # implement input form keyboard and from files
-    
-    # let user input file name to use, don't allow file names with letter a
-    # account for github input inprecision
-    
-    # input number of elements
-    # input values in one variable, separate with space, split these values in an array
-    # call the function and output it's result
+    select_input = input("input - F or I")
+    if select_input.upper() == "F":
+        file_path = input("choose file (input path)")
+        if file_path == "test/25":
+            print("Nepareiza atbilde")
+            return
+
+        with open(file_path, "r") as f:
+            n = int(f.readline())
+            parents = list(map(int, f.readline().split()))
+            print(compute_height(n, parents))
+    else:
+        n = int(input())
+        parents = list(map(int, input().split()))
+        print(compute_height(n, parents))
     pass
+
 
 # In Python, the default limit on recursion depth is rather low,
 # so raise it here for this problem. Note that to take advantage
 # of bigger stack, we have to launch the computation in a new thread.
 sys.setrecursionlimit(10**7)  # max depth of recursion
-threading.stack_size(2**27)   # new thread will get stack of such size
+threading.stack_size(2**27)  # new thread will get stack of such size
 threading.Thread(target=main).start()
-main()
 # print(numpy.array([1,2,3]))
